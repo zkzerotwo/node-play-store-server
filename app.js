48fe4286d4926701fd418635e2055016be04468a
Christopher 'ZK' Hal <halc713@gmail.com>
2021-02-18 18:27:44 -0600

    server searches by genre, if sort is given

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express()
+const apps = require('./playstore');
+// const { sort } = require('./playstore');
+
+app.use(morgan('common'));
+app.use(cors());
+
+// const genres = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card']
+
+const query =
+
+    app.get('/apps', (req, res) => {
+        let sort = req.query.sort
+        let genre = req.query.genres
+        // let results = apps
+
+        // .filter(app =>
+        //     app.App
+        //         .includes(sort.toLowerCase()));
+        console.log(req)
+        // console.log(results);
+        console.log(sort);
+        console.log(genre)
+        if (!['App', 'rating'].includes(sort)) {
+            return res
+                .status(400)
+                .send('Sort must be app or rating')
+        }
+
+        if (sort === 'App') {
+            if (!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(genre)) {
+                return res
+                    .status(400)
+                    .send('Please pick an applicable genre')
+            }
+
+        }
+        let results = apps.filter(app => app.Genres.includes(genre))
+        if (sort) {
+            results
+                .sort((a, b) => {
+                    return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+                });
+        }
+        res
+            .json(results)
+    })
+
+app.listen(8000, () => {
+    console.log('Server started on PORT 8000');
+});
